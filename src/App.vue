ae87dedf828ef2a6436002dbdab0b277595d02c7
John Goyo <jgoyosilva@gmail.com>
2021-05-21 22:42:19 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Bienvenidos a Strike-Out3 App"/>
 </template>
 
 <script>
@@ -16,11 +15,9 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', sans-serif; 
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
